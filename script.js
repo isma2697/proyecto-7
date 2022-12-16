18f124de124d62f730d1c9573e6260a0804a16eb
isma2697 <isma2697@gmail.com>
2022-12-16 11:43:44 +0000

    Implementación

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ class formulario {
         this.xmobile = /^[0-9]{9}$/;
         this.xiban = /^[A-Z]{2}[0-9]{22}$/;
         this.xcreditcard = /^[0-9]{16}$/;
-        /* A regular expression that checks if the password has at least one lowercase letter, one
-        uppercase letter, one number, one special character and is at least 12 characters long. */
-        this.xpassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{12,}$/;
-        this.xrepeatpassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{12,}$/;
+        this.xpassword = /^(?=.[A-Za-z])(?=.\d)(?=.[@$!%#?&])[A-Za-z\d@$!%*#?&]{12,}$/;
+        this.xrepeatpassword = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[^\da-zA-Z]).{8,12}$/;
         this.inputs = document.querySelectorAll('input');
+        this.btn = document.querySelector('#btn');
+        console.log(this.btn);
 
 
         this.inputs.forEach((input) => {
@@ -23,8 +23,6 @@ class formulario {
                 console.log(e.target.value)
                 if (e.target.name == "name") {
                     this.validate(e.target, this.xname)
-                    //cambiar de color el input
-
                 };
                 if (e.target.name == "surname") {
                     this.validate(e.target, this.xsurname)
@@ -61,15 +59,47 @@ class formulario {
                 };
             });
         });
+
+        this.btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log('enviado');
+            let name       = document.querySelector('#name').value;
+            let surname    = document.querySelector('#surname').value;
+            let idcard     = document.querySelector('#dni').value;
+            let date       = document.querySelector('#date').value;
+            let postalcode = document.querySelector('#postalcode').value;
+            let email      = document.querySelector('#email').value;
+            let telephone  = document.querySelector('#telephone').value;
+            let mobile     = document.querySelector('#mobile').value;
+            let iban       = document.querySelector('#iban').value;
+            let creditcard = document.querySelector('#card').value;
+            let password   = document.querySelector('#password').value;
+            let password2  = document.querySelector('#password2').value;
+            let json = {
+                name      : name,
+                surname   : surname,
+                idcard    : idcard,
+                date      : date,
+                postalcode: postalcode,
+                email     : email,
+                telephone : telephone,
+                mobile    : mobile,
+                iban      : iban,
+                creditcard: creditcard,
+                password  : password,
+                password2 : password2
+            }
+            console.log(json);
+            this.saveData(json);
+        });
     }
 
     init() {
         this.validate();
     }
 
-
-
-    validate(field, regex) {
+    validate(field, regex) {  
+        console.log(field, regex);  
         if (regex.test(field.value)) {
             console.log(field);
             field.classList.remove('is-invalid');
@@ -78,6 +108,11 @@ class formulario {
             field.classList.remove('is-valid');
             field.classList.add('is-invalid');
         }
+    }
+
+    saveData(json) {
+        console.log(json);
+        localStorage.setItem('json', JSON.stringify(json));
     }
 
 
